e50c46b65a92db924c1833d3a5a9c9d9402d7a5d
YIFAN ZHU <154294424+Evan2001ok@users.noreply.github.com>
2025-01-05 16:40:50 -0500

    Add files via upload

---
+++ recursion.c
@@ -0,0 +1,24 @@
+//Auther: yifan zhu
+// This is a recursion fuction, calculate sum of total of 100 = 1+2+3+...100
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <stdbool.h>
+
+long sum(long n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return sum(n-1) + n;
+    }
+}
+
+int main(void)
+{
+    printf("3 's total sum is: %d", f(10));
+    return 0;
+}
